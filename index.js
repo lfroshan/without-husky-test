bdeab52390c1e51c6a8fb7594bfd84550d0a54ca
Roshan Shrestha <roshanshrestha1@lftechnology.com>
2022-03-28 12:56:02 +0545

    updated

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 console.log("This is in proper format.");
-console.log("This isn't in proper format.");
+    console.log("This isn't in proper format.")
 
-console.log("hello this is also not properly formatted.");
+
+
+                  console.log(  "hello this is also not properly formatted.")
+
+
+            console.log("This is to be on update without husky. not formatted.")
